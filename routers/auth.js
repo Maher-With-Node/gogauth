b14efbddbe61f66e1308ddd8b74ac891ad8152ed
Maher Salah <email>
2023-03-09 18:16:37 +0200

    edit auth.js

--- routers/auth.js
+++ routers/auth.js
@@ -2,8 +2,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const passport = require('passport');
 const router = express.Router();
-router.get("/google",passport.authenticate("google", {
-scope: ["profile", "email"],
+
+router.get('/google',passport.authenticate('google', {
+scope: ['profile', 'email'],
 })
 );
 ///Callback route for google to redirect
